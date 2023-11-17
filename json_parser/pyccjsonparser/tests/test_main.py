8f9580ea501779c2426bc8cfb9fd5d0f8ae216c9
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2023-11-17 10:46:53 -0500

    save

---
+++ json_parser/pyccjsonparser/tests/test_main.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from pyccjsonparser.main import main
+
+
+def test_file_doesnot_exists(tmp_path: Path):
+    code = main(tmp_path / "non-existing")
+    assert code == 1
+
+
+def test_file_is_invalid_json(tmp_path: Path):
+    file = tmp_path / "test.json"
+    file.write_text("\{")
+    code = main(file)
+    assert code == 2
+
+
+def test_file_is_valid_json(tmp_path: Path):
+    file = tmp_path / "test.json"
+    file.write_text("\{\}")
+    code = main(file)
+    assert code == 0

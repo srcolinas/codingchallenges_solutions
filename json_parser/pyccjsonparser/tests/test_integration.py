158a7be3c6620fbd21b48142721df98a6ddbb3cf
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2023-11-19 17:26:46 -0500

    update

--- json_parser/pyccjsonparser/tests/test_integration.py
+++ json_parser/pyccjsonparser/tests/test_integration.py
@@ -1,18 +1,23 @@
-# import subprocess
-# import sys
-# from pathlib import Path
+import subprocess
+import sys
+from pathlib import Path
 
-# import pytest
+import pytest
 
 
-# @pytest.mark.parametrize(
-#     "file,exit_code",
-#     [
-#         (Path("doesnt-exist.extension"), 1),
-#         (Path("..") / "tests" / "step1" / "valid.json", 0),
-#         (Path("..") / "tests" / "step1" / "invalid.json", 2),
-#     ],
-# )
-# def test(file: Path, exit_code: int):
-#     proc = subprocess.run(["pyccjsonparser", f"{file}"], executable=sys.executable)
-#     assert proc.returncode == exit_code
+@pytest.mark.parametrize(
+    "file,exit_code",
+    [
+        (Path("doesnt-exist.extension"), 1),
+        (Path(".").absolute().parent / "tests" / "step1" / "valid.json", 0),
+        (Path(".").absolute().parent / "tests" / "step1" / "invalid.json", 2),
+    ],
+)
+def test(file: Path, exit_code: int):
+    proc = subprocess.run(
+        ["pyccjsonparser", f"{file}"],
+        executable=sys.executable,
+        capture_output=True,
+        text=True,
+    )
+    assert proc.returncode == exit_code, proc.stderr + proc.stdout
